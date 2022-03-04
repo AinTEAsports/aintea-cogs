73efb2ff14c4f7c87f3d7a61d1694fc0f7e71535
AinTEAsports <benkirat.khalil@gmail.com>
2022-03-04 19:36:01 +0100

    Due to import error, put urlGenerator and webscrapGet content into mycog file

--- asciiart/mycog.py
+++ asciiart/mycog.py
@@ -1,7 +1,61 @@
 from redbot.core import commands
+from selenium import webdriver
 
-import webscrapGet as wG
-import urlGenerator as uG
+"""import webscrapGet as wG
+import urlGenerator as uG"""
+
+
+with open('textPolices.txt', 'r') as f:
+    fontList = f.read()
+
+
+def createUrl(textStyle : str, text : str):
+    """Function that will create URL from style and text
+
+    Args:
+        textStyle (str): the text style you want
+        text (str): the text you want to convert to ASCII art
+
+    Returns:
+        str: the URL where will be scraped the ASCII art
+    """
+    
+    return f"https://patorjk.com/software/taag/#p=display&f={textStyle}&t={text}"
+
+
+def getASCII(siteURL : str):
+    """Function to get ASCII art from a link
+
+    Args:
+        siteURL (str): the link where the function will take
+                        the ASCII art
+
+    Returns:
+        str: the ASCII art the program got from the website
+    """
+
+    # Setting headless mode
+    driverOptions = webdriver.FirefoxOptions()
+    driverOptions.headless = True
+    
+    driver = webdriver.Firefox(options=driverOptions)
+    driver.get(siteURL)
+    
+    # Getting the ASCII art
+    asciiArt = driver.find_element_by_id("taag_output_text")
+
+    # If no ASCII art has been got, the program will return an empty
+    # string
+    if not asciiArt:
+        return ""
+        
+    # Returns ASCII art without HTML balises
+    return asciiArt.text
+
+
+
+
+############ COG CLASS #############
 
 
 class MyCog(commands.Cog):
@@ -28,11 +82,11 @@ class MyCog(commands.Cog):
             await ctx.send("```\nPlease enter text```")
             return
         
-        if style not in wG.fontList:
+        if style not in fontList:
             await ctx.send(f"```\nStyle '{style}' does not exist```")
             return
         
-        url = uG.createUrl(textStyle=style, text=text)
-        asciiArt = wG.getASCII(url)
+        url = createUrl(textStyle=style, text=text)
+        asciiArt = getASCII(url)
         
         await ctx.send(f"```\n{asciiArt}```")
