07fd6ce54ffe8f029ac6a65effe0a304682a1659
AinTEAsports <benkirat.khalil@gmail.com>
2022-03-04 19:03:26 +0100

    Added ascii_art command

--- asciiart/mycog.py
+++ asciiart/mycog.py
@@ -1,5 +1,13 @@
 from redbot.core import commands
 
+import asciiModules.webscrapGet as wG
+import asciiModules.urlGenerator as uG
+
+
+with open('./asciiModules/textPolices.txt', 'r') as f:
+    styleList = f.read()
+
+
 class MyCog(commands.Cog):
     """My custom cog"""
 
@@ -9,7 +17,26 @@ class MyCog(commands.Cog):
     @commands.command()
     async def hi(self, ctx):
         await ctx.send(f"Hi {ctx.author.mention}")
+     
         
     @commands.command()
     async def test(self, ctx):
         await ctx.send("I'm here")
+        
+    
+    @commands.command()
+    async def ascii_art(self, ctx, style : str, *, text : str):
+        text = " ".join(text)
+        
+        if not text:
+            await ctx.send("```\nPlease enter text```")
+            return
+        
+        if style not in styleList:
+            await ctx.send(f"```\nStyle '{style}' does not exist```")
+            return
+        
+        url = uG.createUrl(textStyle=style, text=text)
+        asciiArt = wG.getASCII(url)
+        
+        await ctx.send(f"```\n{asciiArt}```")
