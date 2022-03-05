c14c35c1bc8e0d2c4469d59068ba1613e98827cd
AinTEAsports <benkirat.khalil@gmail.com>
2022-03-05 14:18:11 +0100

    Added content to general_tea/mycog

--- general_tea/mycog.py
+++ general_tea/mycog.py
@@ -1,3 +1,5 @@
+import calendar
+
 from redbot.core import commands
 
 
@@ -8,5 +10,17 @@ class MyCog(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def hi(self, ctx):
-        await ctx.send(f"Hi {ctx.author.mention}")
+    async def calendrier(self, ctx, month : int, year : int) -> None:
+        if not isinstance(month, int) or not isinstance(year, int):
+            return
+        
+        if month < 1 or month > 12:
+            await ctx.send(f"```\nParameter \"month\" ({month}) is invalid```")
+        
+        calendrierText = calendar.month(year, month)
+        await ctx.send(f"```py\n{calendrierText}```")
+    
+    
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"```py\n{commands.bot.latency*1000} ms```")
