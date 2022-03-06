4a3f0e6b47093eafee97f0f6f16417c3e578feb7
AinTEAsports <benkirat.khalil@gmail.com>
2022-03-06 11:23:27 +0100

    Changed imports because of errors

--- asciiart/__init__.py
+++ asciiart/__init__.py
@@ -1,4 +1,6 @@
 from .mycog import MyCog
+from .asciiModules.urlGenerator import *
+from .asciiModules.webscrapGet import *
 
 def setup(bot):
     bot.add_cog(MyCog(bot))
--- asciiart/mycog.py
+++ asciiart/mycog.py
@@ -1,58 +1,12 @@
 from redbot.core import commands
 from selenium import webdriver
 
-"""import webscrapGet as wG
-import urlGenerator as uG"""
+from asciiModules.webscrapGet import getASCII
+from asciiModules.urlGenerator import createUrl
 
 
-with open('textPolices.txt', 'r') as f:
+with open('./asciiModules/textPolices.txt', 'r') as f:
     fontList = f.read()
-
-
-def createUrl(textStyle : str, text : str):
-    """Function that will create URL from style and text
-
-    Args:
-        textStyle (str): the text style you want
-        text (str): the text you want to convert to ASCII art
-
-    Returns:
-        str: the URL where will be scraped the ASCII art
-    """
-    
-    return f"https://patorjk.com/software/taag/#p=display&f={textStyle}&t={text}"
-
-
-def getASCII(siteURL : str):
-    """Function to get ASCII art from a link
-
-    Args:
-        siteURL (str): the link where the function will take
-                        the ASCII art
-
-    Returns:
-        str: the ASCII art the program got from the website
-    """
-
-    # Setting headless mode
-    driverOptions = webdriver.FirefoxOptions()
-    driverOptions.headless = True
-    
-    driver = webdriver.Firefox(options=driverOptions)
-    driver.get(siteURL)
-    
-    # Getting the ASCII art
-    asciiArt = driver.find_element_by_id("taag_output_text")
-
-    # If no ASCII art has been got, the program will return an empty
-    # string
-    if not asciiArt:
-        return ""
-        
-    # Returns ASCII art without HTML balises
-    return asciiArt.text
-
-
 
 
 ############ COG CLASS #############
