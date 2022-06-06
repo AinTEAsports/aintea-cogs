b439fc729fa2521f17538d4aaa0eca7319acf519
AinTEAsports <benkirat.khalil@gmail.com>
2022-06-06 18:31:38 +0200

    mycog.py : removed alias in descriptor

--- asciiart/mycog.py
+++ asciiart/mycog.py
@@ -17,7 +17,7 @@ class AsciiCog(commands.Cog):
 
 
 
-    @cog_ext.cog_slash(name="avatar", aliases=["av"], description="Get your avatar in ASCII art")
+    @cog_ext.cog_slash(name="avatar", description="Get your avatar in ASCII art")
     async def avatar(self, ctx, user : discord.User = None):
         if not user:
             user = ctx.author
